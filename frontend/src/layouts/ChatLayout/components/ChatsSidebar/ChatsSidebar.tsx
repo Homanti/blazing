40f7abfabe605c0bfc0dc13c0db642ff0c920db5
Homanti <homanti0mb2@gmail.com>
2026-02-16 14:06:44 -0800

    redesign

--- frontend/src/layouts/ChatLayout/components/ChatsSidebar/ChatsSidebar.tsx
+++ frontend/src/layouts/ChatLayout/components/ChatsSidebar/ChatsSidebar.tsx
@@ -1,5 +1,7 @@
 import styles from './ChatsSidebar.module.scss';
 import ChatItem from "@/layouts/ChatLayout/components/ChatsSidebar/ChatItem/ChatItem.tsx";
+import {useAuthStore} from "@/stores/authStore.tsx";
+import UserProfilePanel from "@/components/UserProfilePanel/UserProfilePanel.tsx";
 
 const users = [
     {username: "bebra", channelId: "1"},
@@ -10,11 +12,19 @@ const users = [
 ]
 
 function ChatsSidebar() {
+    const account = useAuthStore(s => s.currentAccount);
+
+    if (!account) return null;
+
     return (
         <div className={styles.sidebar}>
-            {users.map((user) => (
-                <ChatItem username={user.username} channelId={user.channelId} />
-            ))}
+            <div className={styles.chats}>
+                {users.map((user) => (
+                    <ChatItem username={user.username} channelId={user.channelId} />
+                ))}
+            </div>
+
+            <UserProfilePanel user={account.user} />
         </div>
     );
 }
